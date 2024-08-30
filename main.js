2924a3816db6bf376edeca8f2f407947a4c4bd92
Yosihub <ykathalina4@gmail.com>
2024-08-29 21:42:57 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function toggleDarkMode() {
-    const darkModeStylesheet = document.getElementById('dark-mode');
-    if (darkModeStylesheet.disabled) {
-        darkModeStylesheet.disabled = false;
-    } else {
-        darkModeStylesheet.disabled = true;
-    }
-}
